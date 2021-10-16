8096108bbed8c552dff538579cc1f1c61048d6fd
edinelsonslima <edinelsonlima86@gmail.com>
2021-10-16 03:54:52 -0300

    mais um

---
+++ src/components/FormLogin/index.tsx
@@ -0,0 +1,120 @@
+import facebookImg from '../../assets/facebook.png';
+import googleImg from '../../assets/google.png';
+import githubImg from '../../assets/github.png';
+import { Link } from 'react-router-dom';
+import './index.css';
+import { FormEvent, MouseEventHandler, useMemo, useState } from 'react';
+
+interface FormLoginProps {
+    email: string;
+    pass: string;
+    setEmail: (arg0: string) => void;
+    setPass: (arg0: string) => void;
+    submit: (arg0: FormEvent<HTMLFormElement>) => void;
+    singInGoogle?: MouseEventHandler<HTMLDivElement>;
+    singInFacebook?: MouseEventHandler<HTMLDivElement>;
+    singInGithub?: MouseEventHandler<HTMLDivElement>;
+    create?: boolean;
+}
+
+const FromLogin = (props: FormLoginProps) => {
+    const [styleCreate, setStyleCreate] = useState({});
+    useMemo(() => {
+        if (props.create) setStyleCreate({ display: 'none' });
+    }, [props.create]);
+
+    return (
+        <section className='login-container'>
+            <div className='login-filter-blur'></div>
+            <div className='login-content'>
+                <h1 className='login-title'>{props.create? 'Create Account' :'Music App'}</h1>
+                <form
+                    className='login-form'
+                    onSubmit={(e: React.FormEvent<HTMLFormElement>) =>
+                        props.submit(e)
+                    }
+                >
+                    <input
+                        type='email'
+                        className='login-email'
+                        value={props.email}
+                        onChange={(e) => props.setEmail(e.target.value)}
+                        placeholder='Email'
+                        required
+                    />
+
+                    <input
+                        type='password'
+                        className='login-pass'
+                        value={props.pass}
+                        onChange={(e) => props.setPass(e.target.value)}
+                        placeholder='Password'
+                        required
+                    />
+
+                    <div className='login-links' style={styleCreate}>
+                        <Link
+                            className='login-create-account'
+                            to={'/create-user'}
+                        >
+                            create account
+                        </Link>
+                        <Link
+                            className='login-create-account'
+                            to={'/create-user'}
+                        >
+                            Forgot your password?
+                        </Link>
+                    </div>
+                    <div
+                        className='login-buttons'
+                        style={
+                            !props.create ? { 
+                                justifyContent: 'center',
+                                marginTop: '20px'
+                            } : {}
+                        }
+                    >
+                        <Link
+                            className='login-button'
+                            to='/login'
+                            style={!props.create ? { display: 'none' } : {}}
+                        >
+                            Voltar
+                        </Link>
+                        <input
+                            type='submit'
+                            className='login-button'
+                            value={props.create ? 'Cadastrar' : 'Entrar'}
+                        />
+                    </div>
+                </form>
+                <span className='login-social-or' style={styleCreate}>
+                    or
+                </span>
+                <div className='login-social' style={styleCreate}>
+                    <div
+                        className='login-icon-social'
+                        onClick={props.singInGoogle}
+                    >
+                        <img src={googleImg} alt='google icon' />
+                    </div>
+                    <div
+                        className='login-icon-social'
+                        onClick={props.singInFacebook}
+                    >
+                        <img src={facebookImg} alt='facebook icon' />
+                    </div>
+                    <div
+                        className='login-icon-social'
+                        onClick={props.singInGithub}
+                    >
+                        <img src={githubImg} alt='github icon' />
+                    </div>
+                </div>
+                <section className='login-app'></section>
+            </div>
+        </section>
+    );
+};
+export { FromLogin };
